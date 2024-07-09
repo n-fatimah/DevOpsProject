e48004a1cec0a30b18789308d9859051e3588304
Noor Fatimah <noor.fatimah@camp1.tkxel.com>
2024-07-09 10:17:39 +0500

    Update response message in app.js

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ const port = 3000;
 
 // Start the server and listen on the defined port
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}/`);
+  console.log(`your Server is running at http://localhost:${port}/`);
 });
