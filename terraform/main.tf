7645e8c822e453c990c8f6dfdb02968a3d85cfbd
Noor Fatimah <noor.fatimah@camp1.tkxel.com>
2024-07-10 10:41:16 +0500

    updated main.tf for public ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_instance" "example" {
@@ -16,9 +17,10 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"    
 
   subnet_id = aws_subnet.public.id
+  associate_public_ip_address = true
 
   tags = {
-    Name = "HelloWorldInstance"
+    Name = "HelloWorldInstance2"
   }
 }
 
